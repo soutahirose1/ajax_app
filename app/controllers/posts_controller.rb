f0243601e24c25df3a10dda82430475630af1d3d
soutahirose1 <71875688+soutahirose1@users.noreply.github.com>
2020-10-07 11:51:03 +0900

    既読機能のフロント実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,22 @@
 class PostsController < ApplicationController
 
   def index 
-   @posts = Post.all.order(id: "DESC")
+    @posts = Post.all.order(id: "DESC")
   end
 
   def create
     Post.create(content: params[:content])
+    redirect_to action: :index
+
   end
 
-  # checkedアクションは既読の操作を行った時に実行されるアクション
   def checked
-    
     post = Post.find(params[:id])
-    if post.checked 
+    if post.checked then
       post.update(checked: false)
     else
       post.update(checked: true)
     end
-
     item = Post.find(params[:id])
     render json: { post: item }
   end
